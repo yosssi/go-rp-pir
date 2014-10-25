0cc03abeee7f7e5620db915fbcfebeb9544f13f8
yosssi <yoshida.keiji.84@gmail.com>
2014-10-25 02:55:02 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		os.Stderr.WriteString("GPIO No should be specified.\n")
+		os.Exit(1)
+	}
+
+	gpio := os.Args[1]
+
+	fexport, err := os.Create("/sys/class/gpio/export")
+	if err != nil {
+		panic(err)
+	}
+
+	defer fexport.Close()
+
+	fexport.WriteString(gpio)
+
+	fdirection, err := os.Create(fmt.Sprintf("/sys/class/gpio/gpio%s/direction", gpio))
+
+	defer fdirection.Close()
+
+	fdirection.WriteString("out")
+
+	fvalue, err := os.Create(fmt.Sprintf("/sys/class/gpio/gpio%s/value", gpio))
+	if err != nil {
+		panic(err)
+	}
+
+	defer fvalue.Close()
+
 	if err := rpio.Open(); err != nil {
 		panic(err)
 	}
@@ -21,7 +51,10 @@ func main() {
 
 	for {
 		if pin.Read() == rpio.High {
+			fvalue.WriteString("1")
 			fmt.Println("Motion Detected!")
+		} else {
+			fvalue.WriteString("0")
 		}
 
 		time.Sleep(1 * time.Second)
